00367d6b8a2aff3609dc0f9bce7731c30db480da
Maite Martínez <105754123+staruniverse2050@users.noreply.github.com>
2022-11-04 14:34:13 -0500

    Add files via upload

---
+++ Examen/app.js
@@ -0,0 +1,30 @@
+const inputName= document.getElementById("txtname");
+const btncalculate = document.getElementById("calculate");
+const note1 = document.getElementById("Note1");
+const note2 = document.getElementById("Note2");
+const note3 = document.getElementById("Note3");
+const btnaverage = document.getElementById("average");
+
+btncalculate.addEventListener("click",(event) => {
+    let nameUser = inputName.value;
+    let NoteOne = note1.value;
+    let NoteTwo = note2.value;
+    let NoteThree = note3.value;
+    let percentage1 = NoteOne*30/100;
+    let percentage2 = NoteTwo*30/100;
+    let percentage3 = NoteThree*40/100;
+    let average = (((parseFloat(NoteOne+percentage1)+parseFloat(NoteTwo+percentage2)+parseFloat(NoteThree+percentage3))/3));
+    let totallyaverage = average.toFixed(1);
+    if(totallyaverage >= 4.5){
+        btnaverage.textContent = `${nameUser} según sus notas, su promedio es EXCELENTE y es de: ${totallyaverage} , ganó la materia`;
+    }
+    else if(totallyaverage >= 3.5 && totallyaverage <= 4.5){
+        btnaverage.textContent = `${nameUser} según sus notas, su promedio es BUENO y es de: ${totallyaverage} , ganó la materia`;
+    }
+    else if(totallyaverage >= 2.5 && totallyaverage <= 3.4){
+        btnaverage.textContent = `${nameUser} según sus notas, su promedio es REGULAR y es de: ${totallyaverage} , perdió la materia, pero cuenta con la oportunidad de recuperarla`;
+    }
+    else if(totallyaverage <= 2){
+        btnaverage.textContent = `${nameUser} según sus notas, su promedio es MALO y es de: ${totallyaverage} , perdió la materia, debe repetirla`;
+    
+    });
